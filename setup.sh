17829ea095ba070b023905f8b754e902683530d2
degrigis <degrigis@ucsb.edu>
2024-01-07 18:55:37 -0800

    added setup
    Signed-off-by: degrigis <degrigis@ucsb.edu>

---
+++ setup.sh
@@ -0,0 +1,42 @@
+
+#!/bin/bash
+# set -x
+
+# CURRENT-WORKDIR: jackal
+read -p "Press continue if you are in a python virtuale env: " ANSWER
+
+apt-get update 
+apt-get install -y htop tmux git vim nano\ 
+                   wget cmake python3-pip wget\
+                   graphviz libgraphviz-dev libffi-dev python3.11-dev python3.11\
+                   gperf libgmp-dev mkisofs bison clang doxygen flex libncurses5-dev\ 
+                   libsqlite3-dev mcpp libboost-all-dev time
+
+# get the WEB3_PROVIDER url from user
+read -p "Enter WEB3_PROVIDER url: " WEB3_PROVIDER
+
+git clone https://github.com/ethpwn/ethpwn.git
+cd ethpwn && pip install -e .
+ethpwn wallet create
+
+rm ~/.config/ethpwn/config.json
+
+echo "{
+  \"default_network\": \"mainnet\",
+  \"default_node_urls\": {
+    \"mainnet\": \"$WEB3_PROVIDER\",
+  },
+}" > ~/.config/ethpwn/config.json
+
+
+# back to jackal folder
+cd ..
+git clone https://github.com/ucsb-seclab/greed.git
+cd greed
+git checkout fae1812180ff2ffe4b0a789f84071988b0917745 # release 1.0.0
+echo "OK" | ./setup.sh
+
+# export the WEB3_PROVIDER as environment variable
+export WEB3_PROVIDER=$WEB3_PROVIDER
+
+echo "DONE!"
